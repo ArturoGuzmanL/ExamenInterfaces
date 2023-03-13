ec6c2c4528349a39b38992322e66cc76207350ca
Arturo Guzmán Lucena <93922359+ArturoGuzmanL@users.noreply.github.com>
2023-03-13 19:11:24 +0100

    Examen finalizado

--- examenInterfaces/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module main.exameninterfaces {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens main.exameninterfaces to javafx.fxml;
-    exports main.exameninterfaces;
-}
